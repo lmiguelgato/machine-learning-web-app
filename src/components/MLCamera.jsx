a44d9ea86da3a1d19bf50a132e05cd89c7000acc
Luis M. Gato Díaz <lmiguelgato@gmail.com>
2021-04-01 18:53:07 -0600

    Convert data URI to ImageData

--- src/components/MLCamera.jsx
+++ src/components/MLCamera.jsx
@@ -3,23 +3,40 @@ import Button from 'react-bootstrap/Button'
 import Webcam from "react-webcam";
 import * as tf from '@tensorflow/tfjs';
 
+
 function MLCamera(props) {
   const [capture, setCapture] = useState(false);
-  const [batchedImage, setBatchedImage] = useState(tf.tensor([[[1, 2], [3, 4]], [[1, 2], [3, 4]], [[1, 2], [3, 4]]]));
 
   const webcamRef = React.useRef(null);
 
   const takePicture = React.useCallback(
     () => {
       const imageSrc = webcamRef.current.getScreenshot();
-      console.log(imageSrc)
-      //const tensorImage = tf.browser.fromPixels(imageSrc);
-      //const reversedImage = tensorImage.reverse(1);
-      //const croppedImage = cropImage(reversedImage);
-      //setBatchedImage(croppedImage.expandDims(0).toFloat().div(tf.scalar(127)).sub(tf.scalar(1)));
+      convertURIToImageData(imageSrc).then(function(imageData) {
+        const tensorImage = tf.browser.fromPixels(imageData);
+        const reversedImage = tensorImage.reverse(1);
+        const croppedImage = cropImage(reversedImage);
+        const batchedImage = croppedImage.expandDims(0).toFloat().div(tf.scalar(127)).sub(tf.scalar(1));
+      });
     },
     [webcamRef]
   );
+
+  function convertURIToImageData(URI) {
+    return new Promise(function(resolve, reject) {
+      if (URI == null) return reject();
+      var canvas = document.createElement('canvas'),
+          context = canvas.getContext('2d'),
+          image = new Image();
+      image.addEventListener('load', function() {
+        canvas.width = image.width;
+        canvas.height = image.height;
+        context.drawImage(image, 0, 0, canvas.width, canvas.height);
+        resolve(context.getImageData(0, 0, canvas.width, canvas.height));
+      }, false);
+      image.src = URI;
+    });
+  }
 
   const cropImage = (img) => {
     const size = Math.min(img.shape[0], img.shape[1]);
