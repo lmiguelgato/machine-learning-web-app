56e9fd39f50526eca36de6daa9013d4bdfc4ae6f
Luis M. Gato Díaz <lmiguelgato@gmail.com>
2021-04-02 19:25:08 -0600

    Update npm test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders web page title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Machine Learning web app 💡/i);
   expect(linkElement).toBeInTheDocument();
 });
