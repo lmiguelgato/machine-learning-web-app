06fbf4c59351923d4fd6ecc03bf363f6813324a5
Luis M. Gato <lmiguelgato@gmail.com>
2021-04-09 20:42:02 -0500

    Refactor, comment, and set up for repeatability

--- api/benchmark/bench_tf.py
+++ api/benchmark/bench_tf.py
@@ -1,67 +1,93 @@
-""" Benchmark to check tensorflow is properly installed.
-    Particularly useful for macs with Apple Silicon chips, where TF is not fully functional yet.
-    Originally from: https://github.com/apple/tensorflow_macos/issues/25
+""" Benchmark to check TensorFlow is properly installed. Particularly useful for macs with Apple
+    Silicon chips, where TensorFlow is not fully functional and stable yet.
+
+    This benchmark consists of training a deep neural network, including two convolutional layers
+    and two fully-connected layers, with max-pooling and dropout in between. It is the classical
+    solution to the digits classifier for the MNIST dataset.
+
+    The model is defined through the Sequential API from Keras, and the MNIST dataset is loaded
+    from `tensorflow_datasets`: a collection of public research datasets that enable easy-to-use
+    and high-performance input pipelines through the Data API from TensorFlow.
+
+    Benchmark popularized thanks to this issue: https://github.com/apple/tensorflow_macos/issues/25
 """
 import tensorflow as tf
 import tensorflow_datasets as tfds
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import (
+    Conv2D,
+    MaxPooling2D,
+    Dropout,
+    Flatten,
+    Dense
+    )
 
-# from tensorflow.python.framework.ops import disable_eager_execution
-# disable_eager_execution()
+# Set the global random seed for repeatability:
+SEED = 42
+tf.random.set_seed(SEED)
 
-# from tensorflow.python.compiler.mlcompute import mlcompute
-# mlcompute.set_mlc_device(device_name='gpu')
+# Hyperparameters for training:
+NUM_EPOCHS = 12
+BATCH_SIZE = 128
+LEARNING_RATE = 0.001
 
+# Enable auto-tuning dataset operations:
+AUTO = tf.data.experimental.AUTOTUNE
 
-epochs = 12
-batch_size = 128
-learning_rate = 0.001
-
-
-def normalize_img(image, label):
-    """Normalizes images: `uint8` -> `float32`."""
-    return tf.cast(image, tf.float32) / 255., label
-
-
+# Load MNIST dataset as `tf.data.Dataset` objects, and split it in train/test sets:
 (ds_train, ds_test), ds_info = tfds.load(
     'mnist',
     split=['train', 'test'],
-    shuffle_files=True,
     as_supervised=True,
     with_info=True,
 )
 
-ds_train = ds_train.map(normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-ds_train = ds_train.cache()
-ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
-ds_train = ds_train.batch(batch_size)
-ds_train = ds_train.prefetch(tf.data.experimental.AUTOTUNE)
 
-ds_test = ds_test.map(normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-ds_test = ds_test.batch(batch_size)
-ds_test = ds_test.cache()
-ds_test = ds_test.prefetch(tf.data.experimental.AUTOTUNE)
+# Preprocessing function to keep images in the same scale and help gradient descent convergence:
+def scale(image, label):
+    """Normalizes and cast images: `uint8` -> `float32`
+    """
+    return tf.cast(image, tf.float32) / 255., label
 
-model = tf.keras.models.Sequential(
+
+# Pipeline for the train dataset: preprocess, cache, shuffle, batch, and prefetch:
+ds_train = ds_train.map(scale, num_parallel_calls=AUTO)  # autotune based on avail. CPU
+ds_train = ds_train.cache()  # the 1st time the dataset is iterated over, its content will be cached
+ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples, SEED)  # shuffles the dataset
+ds_train = ds_train.batch(BATCH_SIZE)  # combine consecutive elements of this dataset into batches
+ds_train = ds_train.prefetch(AUTO)  # autotune the buffer size when prefetching
+
+# Do the same for the test dataset, except for shuffling since we're using the whole test set anyway
+ds_test = ds_test.map(scale, num_parallel_calls=AUTO).cache().batch(BATCH_SIZE).prefetch(AUTO)
+
+# Define a CNN through the Sequential API from Keras:
+model = Sequential(
     [
-        tf.keras.layers.Conv2D(32, kernel_size=(3, 3), activation='relu'),
-        tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu'),
-        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
-        tf.keras.layers.Dropout(0.25),
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dropout(0.5),
-        tf.keras.layers.Dense(10, activation='softmax')
+        Conv2D(32, kernel_size=(3, 3), activation='relu'),
+        Conv2D(64, kernel_size=(3, 3), activation='relu'),
+        MaxPooling2D(pool_size=(2, 2)),
+        Dropout(0.25),
+        Flatten(),
+        Dense(128, activation='relu'),
+        Dropout(0.5),
+        Dense(10, activation='softmax')
     ]
     )
 
+# Configure the model for training:
 model.compile(
     loss='sparse_categorical_crossentropy',
-    optimizer=tf.keras.optimizers.Adam(learning_rate),
+    optimizer=Adam(LEARNING_RATE),
     metrics=['accuracy'],
 )
 
+# Train and evaluate the model for `NUM_EPOCHS` iterations on the dataset:
 model.fit(
     ds_train,
-    epochs=epochs,
+    epochs=NUM_EPOCHS,
     validation_data=ds_test,
 )
+
+# If left the model, dataset, seed, and hyperparameters unchanged, the classification accuracy at
+# the end of the 12th epoch should be 85.27% and 92.83% on the training and test sets, respectively.
